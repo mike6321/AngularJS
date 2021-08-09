753d323e8022770f6399efc6b968363590150a9f
mike6321 <mike6321@naver.com>
2021-08-09 13:31:48 +0900

    (JavaScript) let, const 공통사항

---
+++ JavaScript/block socped variable_let, const 공통사항.js
@@ -0,0 +1,18 @@
+// var a = 0;
+// var b = 1;
+// console.log(a);
+
+// let a = 2;
+// const b = 2;
+
+var a = 10;
+console.log(window.a);
+console.log(a);
+delete a;
+
+/*
+* var 사용 시 전역변수를 조심하자...!
+* */
+
+
+
