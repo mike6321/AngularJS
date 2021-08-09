daeb4948aa7c6b98a3f4961ff3acd147a753c23e
mike6321 <mike6321@naver.com>
2021-08-09 12:20:31 +0900

    (JavaScript) 모든 문 형태에 적용

---
+++ JavaScript/block scope_모든 문 형태에 적용.js
@@ -0,0 +1,23 @@
+{
+    let a = 2;
+    if (a > 1) {
+        let b = a * 3;
+        console.log(b);
+    } else {
+        let b = a / 3;
+        console.log(b);
+    }
+    // console.log(b);
+}
+// console.log(a);
+
+var sum = 0;
+for (let i = 1; i <= 10; i++) { // var (x)
+    sum += i;
+}
+console.log(i);
+console.log(sum);
+
+
+
+
