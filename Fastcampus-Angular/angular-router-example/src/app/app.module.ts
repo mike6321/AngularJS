79f68e4435821134c50179fa0eac59cc7bd2268d
mike6321 <mike6321@naver.com>
2021-08-23 13:38:46 +0900

    (Fastcampus-Angular) 라우팅 사용하기

---
+++ Fastcampus-Angular/angular-router-example/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import {Route, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+// const routes: Route[] = [
+//   {path: '', component: HomeComponent},
+//   {path:"login", component: LoginComponent}
+// ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    // RouterModule.forRoot(routes)
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
