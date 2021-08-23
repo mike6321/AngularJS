3a94341cc848934e7a604bc126d082e62647331b
mike6321 <mike6321@naver.com>
2021-08-23 11:47:45 +0900

    (Fastcampus-Angular) Directive 사용하기

---
+++ Fastcampus-Angular/angular-directive-example/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  isVisible = true;
+  list: {title: string}[] = [
+    {title: 'Tour of Heroes'},
+    {title: 'CLI Documentation'},
+    {title: 'Angular blog'}
+  ];
+
+  style= {
+    color: 'green'
+  };
+}
