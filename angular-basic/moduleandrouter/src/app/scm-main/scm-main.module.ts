e2968144a20524addfd8b6baa962dd5b62fe6efb
mike6321 <mike6321@naver.com>
2021-08-31 18:11:22 +0900

    (moduleandrouter) 기능구현

--- angular-basic/moduleandrouter/src/app/scm-main/scm-main.module.ts
+++ angular-basic/moduleandrouter/src/app/scm-main/scm-main.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import {FooterComponent} from "./footer/footer.component";
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
+  declarations: [NavbarComponent, SidebarComponent, FooterComponent, MainDashboardComponent, PageNotFoundComponent],
   /**
    * 모듈이 분리되어있기 떄문에 exports가 필수적이다!
    * */
-  exports: [NavbarComponent, SidebarComponent, FooterComponent]
+  exports: [NavbarComponent, SidebarComponent, FooterComponent, MainDashboardComponent, PageNotFoundComponent]
 })
 export class ScmMainModule { }
