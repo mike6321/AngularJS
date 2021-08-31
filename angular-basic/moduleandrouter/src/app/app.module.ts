db57584766c8a455d1c040a75bb5f9c859cd9383
mike6321 <mike6321@naver.com>
2021-08-31 17:47:49 +0900

    (moduleandrouter) 기본 레이아웃 구현

--- angular-basic/moduleandrouter/src/app/app.module.ts
+++ angular-basic/moduleandrouter/src/app/app.module.ts
@@ -7,11 +7,17 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {ProductModule} from "./product/product.module";
 import {CategoryModule} from "./category/category.module";
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     /*Angular Module*/
